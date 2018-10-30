99959ea7ab84ad548e2cf5bfeb6734df0d8b9c6a
Soro Cat <sorousha326@gmail.com>
2018-10-30 13:50:49 -0700

    add js, bootstrap, and css

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var saySomething = function(whatToSay) {
+  alert(whatToSay);
+};
+
+var add = function(number1, number2) {
+	return number1 + number2;
+};
+
+var number1 = parseInt (prompt("6"));
+var number2 = parseInt (prompt("2"));
+
+alert(add(number1,number2));
+
+var remainder = function(number1, number2, number3) {
+	return number1 + number2 + number3;
+};
+
+saySomething("hi Jimmy");
+add(6, 3);
+var result = add(6, 3)
+//alert(remainder(6, 3, 4));
+var outputText = "The sum is " + result + ".";
+saySomething(outputText);
